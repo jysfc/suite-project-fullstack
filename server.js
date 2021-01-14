faa3a204df10acc488576fd576289207f0dbaf72
jysfc <yaoj415@gmail.com>
2021-01-14 01:08:55 -0800

    added nodemon, express, dotenv

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 
 app.use("/api/v1/users", require("./api/v1/users"));
-app.use("/api/v1/properties", require("./api/v1/properties"));
-app.use("/api/v1/suites", require("./api/v1/suites"));
+// app.use("/api/v1/properties", require("./api/v1/properties"));
+// app.use("/api/v1/suites", require("./api/v1/suites"));
 app.get("/", (req, res) => res.send("Suite Project"));
 
 const port = process.env.PORT || 3021;
