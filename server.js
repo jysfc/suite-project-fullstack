0ea75afe979c43919edc735fab1c2e5800236759
jysfc <yaoj415@gmail.com>
2021-01-19 22:01:08 -0800

    adding code to deploy to heroku

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use("/api/v1/users", require("./api/v1/users"));
 // app.use("/api/v1/properties", require("./api/v1/properties"));
 app.use("/api/v1/suites", require("./api/v1/suites"));
-app.get("/", (req, res) => res.send("Suite Project"));
+
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const port = process.env.PORT || 3021;
 app.listen(port, () =>
