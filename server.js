27539dd0487c61068a22ebb3286fb8a195b78cba
jysfc <yaoj415@gmail.com>
2021-01-20 21:40:09 -0800

    36min in 352a added express.json to fix route post

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+app.use(express.json());
+
 app.use("/api/v1/users", require("./api/v1/users"));
 // app.use("/api/v1/properties", require("./api/v1/properties"));
 app.use("/api/v1/suites", require("./api/v1/suites"));
