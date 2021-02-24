b4dc0d52ccf917af852d288080334ab030d62ae9
jysfc <yaoj415@gmail.com>
2021-02-23 18:52:33 -0800

    refactor payload

--- client/src/components/pages/SelectProperty.js
+++ client/src/components/pages/SelectProperty.js
@@ -53,24 +53,10 @@ class SelectProperty extends React.Component {
 
    addProperty() {
       console.log("ADD UPDATE_EDITABLE_PROPERTY");
+      const newProperty = { ...this.props.currentUser };
       this.props.dispatch({
          type: actions.UPDATE_EDITABLE_PROPERTY,
-         payload: {
-            propertyId: this.props.currentUser.propertyId,
-            // id: userPropertySuite.suite_id,
-            // title: userPropertySuite.suite_title,
-            // image: userPropertySuite.image,
-            // squareFt: userPropertySuite.square_ft,
-            // maxGuest: userPropertySuite.max_guest,
-            // totalKingBed: userPropertySuite.total_king_bed,
-            // totalQueenBed: userPropertySuite.total_queen_bed,
-            // totalFullBed: userPropertySuite.total_full_bed,
-            // hasWiFi: userPropertySuite.has_wifi,
-            // hasTv: userPropertySuite.has_tv,
-            // hasSafe: userPropertySuite.has_safe,
-            // isAccessible: userPropertySuite.is_accessible,
-            // isActive: userPropertySuite.suite_is_active,
-         },
+         payload: newProperty,
       });
       this.props.history.push("/edit-property");
    }
