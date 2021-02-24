f554754fd2f3fa5963e328848348a17c556fd49d
jysfc <yaoj415@gmail.com>
2021-02-23 18:22:27 -0800

    added filter and uniqby

--- api/v1/properties.js
+++ api/v1/properties.js
@@ -14,8 +14,8 @@ router.get("/", (req, res) => {
    db.query(selectUserPropertySuites, user)
       .then((userPropertySuites) => {
          // console.log(userPropertySuites);
-         const formattedUserPropertySuites = userPropertySuites.map(
-            (userPropertySuite) => {
+         const formattedUserPropertySuites = uniqBy(
+            userPropertySuites.map((userPropertySuite) => {
                return {
                   id: userPropertySuite.user_id,
                   email: userPropertySuite.email,
@@ -23,7 +23,9 @@ router.get("/", (req, res) => {
                   isActive: userPropertySuite.user_is_active,
                   properties: uniqBy(
                      userPropertySuites.map((userPropertySuite) => {
+                        const currentPropertyId = userPropertySuite.property_id;
                         return {
+                           userId: userPropertySuite.user_id,
                            name: userPropertySuite.property_name,
                            id: userPropertySuite.property_id,
                            website: userPropertySuite.website,
@@ -43,41 +45,52 @@ router.get("/", (req, res) => {
                            hasSpa: userPropertySuite.has_spa,
                            isSmokeFree: userPropertySuite.is_smoke_free,
                            isActive: userPropertySuite.property_is_active,
-                           suites: userPropertySuites.map(
-                              (userPropertySuite) => {
-                                 return {
-                                    id: userPropertySuite.suite_id,
-                                    title: userPropertySuite.suite_title,
-                                    image: userPropertySuite.image,
-                                    squareFt: userPropertySuite.square_ft,
-                                    maxGuest: userPropertySuite.max_guest,
-                                    totalKingBed:
-                                       userPropertySuite.total_king_bed,
-                                    totalQueenBed:
-                                       userPropertySuite.total_queen_bed,
-                                    totalFullBed:
-                                       userPropertySuite.total_full_bed,
-                                    hasWiFi: userPropertySuite.has_wifi,
-                                    hasTv: userPropertySuite.has_tv,
-                                    hasSafe: userPropertySuite.has_safe,
-                                    isAccessible:
-                                       userPropertySuite.is_accessible,
-                                    isActive: userPropertySuite.suite_is_active,
-                                 };
-                              }
+                           suites: uniqBy(
+                              userPropertySuites
+                                 .map((userPropertySuite) => {
+                                    return {
+                                       propertyId:
+                                          userPropertySuite.property_id,
+                                       id: userPropertySuite.suite_id,
+                                       title: userPropertySuite.suite_title,
+                                       image: userPropertySuite.image,
+                                       squareFt: userPropertySuite.square_ft,
+                                       maxGuest: userPropertySuite.max_guest,
+                                       totalKingBed:
+                                          userPropertySuite.total_king_bed,
+                                       totalQueenBed:
+                                          userPropertySuite.total_queen_bed,
+                                       totalFullBed:
+                                          userPropertySuite.total_full_bed,
+                                       hasWiFi: userPropertySuite.has_wifi,
+                                       hasTv: userPropertySuite.has_tv,
+                                       hasSafe: userPropertySuite.has_safe,
+                                       isAccessible:
+                                          userPropertySuite.is_accessible,
+                                       isActive:
+                                          userPropertySuite.suite_is_active,
+                                    };
+                                 })
+                                 .filter((suite) => {
+                                    if (suite.propertyId === currentPropertyId)
+                                       return true;
+                                    else return false;
+                                 }),
+                              "id"
                            ),
                         };
                      }),
-                     "userPropertySuite.property_id"
+                     "id"
                   ),
                };
-            }
+            }),
+            "id"
          );
          // console.log(formattedUserPropertySuites);
-         const uniqUsers = uniqBy(formattedUserPropertySuites, "id");
-         const user = uniqUsers[0];
-         console.log(user);
-         return res.status(200).json(user);
+         console.log(JSON.stringify(formattedUserPropertySuites, null, 3));
+         const formattedUserPropertySuite = formattedUserPropertySuites[0];
+         console.log(formattedUserPropertySuite);
+         return res.status(200).json(formattedUserPropertySuite);
       })
 
       .catch((err) => {
